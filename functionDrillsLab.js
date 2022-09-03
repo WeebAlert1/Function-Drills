56d3e73e198264a1f3a60cea8991ea0015fe91da
Keith Holdaway <keithholdaway1@hotmail.com>
2022-09-03 14:48:49 -0700

    kinda finished

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -375,13 +375,25 @@ console.log(totalFrogs2);
 
 ////////////////// PROBLEM 21 ////////////////////
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray2 = [0,1,2,3,4,5,6,7,8]
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
 //CODE HERE
+function arrything(arr){
+  const comp = arr[0];
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] <= comp){
+      return false
+    } else {
+      comp = arr[i];
+    }
+  }
+  return true;
+}
 
-
+console.log(arrything(sampleArray2));
 ////////////////// PROBLEM 22 ////////////////////
 
 let duck = "cute";
